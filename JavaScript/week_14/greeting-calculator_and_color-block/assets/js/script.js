5102ae8251b0ed5c965d5fea757129fab760f05b
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2022-11-09 03:50:30 +0300

    added gallery js

--- JavaScript/week_14/greeting-calculator_and_color-block/assets/js/script.js
+++ JavaScript/week_14/greeting-calculator_and_color-block/assets/js/script.js
@@ -74,8 +74,25 @@ function addition(){
 
   //gallery
 
-  function mountains(){
-    let mountains = document.getElementById('mountains');
-    mountains.src = './assets/images/image_2.jpg';
-  }
+  let photo = document.querySelectorAll('#gallery__photos .gallery__item img');
+  let prev = document.querySelector('.prev');
+  let next = document.querySelector('.next');
+  let i = 0;
   
+  prev.onclick = function(){
+    photo[i].style.display = 'none';
+    i--;
+    if(i < 0) {
+      i = photo.length - 1;
+    }
+    photo[i].style.display = 'block';
+  }
+
+  next.onclick = function(){
+    photo[i].style.display = 'none';
+    i++;
+    if(i >= photo.length) {
+      i = 0;
+    }
+    photo[i].style.display = 'block';
+  }
