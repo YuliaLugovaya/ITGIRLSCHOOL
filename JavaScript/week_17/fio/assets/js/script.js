a7f4b47adf6ee9c26f9ee62ca28373cb01910f54
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2022-12-01 20:49:38 +0300

    added some js for fio

--- JavaScript/week_17/fio/assets/js/script.js
+++ JavaScript/week_17/fio/assets/js/script.js
@@ -1,18 +1,37 @@
+let fio = document.querySelector('#fio');
+let lastNameResult = document.querySelector('#lastName');
+let personResult = document.querySelector('#name');
+let patronymicResult = document.querySelector('#patronymic');
+
 function check(){
-    let fio = document.querySelector('#fio');
+    
     let error = document.querySelector('#errorFio');
-    let form = document.querySelector('.form__container');
+    let arr = fio.value.split(" ");
   
     if(fio.value === ''){
       error.innerHTML = 'Укажите Фамилию, имя и отчество.';
       fio.style.border = '2px solid rgb(197, 0, 0)';
     }
-  
-    if(person.value !== '' && login.value !== '' && email.value !== '' && password.value.length >=6 && tel.value.length == 13 && age.value >=14){
-      form.style.display = 'none';
-      document.querySelector('.hello').innerHTML = `Добро пожаловать, ${person.value}!`;
-    }
+
+    let lastName = arr[0];
+    let person = arr[1];
+    let patronymic = arr[2];
+
+    lastNameResult.value = lastName;
+    personResult.value = person;
+    patronymicResult.value = patronymic;
+
+  }
+
+  function clear(){
+    fio.value = '';
+    lastNameResult.value = '';
+    personResult.value = '';
+    patronymicResult.value = '';
   }
   
   let btn = document.querySelector(".form__button");
   btn.addEventListener("click", check);
+
+  let reset = document.querySelector(".form__button_reset");
+  reset.addEventListener("click", clear);
