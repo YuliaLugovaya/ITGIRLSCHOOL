55f6ebb49f4a31abcfa875af3b5779b029777611
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2022-12-02 17:38:54 +0300

    added js for anonim

--- JavaScript/week_17/anonim/assets/js/script.js
+++ JavaScript/week_17/anonim/assets/js/script.js
@@ -3,15 +3,22 @@ let newMessage = document.querySelector('.textarea');
 let result = document.querySelector('.field');
 
 
-function addMessage() {
+function checkSpam() {
     let newMessageValue = newMessage.value;
     newMessage.value = '';
     messages.push(newMessageValue);
     result.innerHTML = '';
+
     for (let message of messages) {
-        result.innerHTML += `<div><p>${message}</p></div>`;
-        }     
+        if(message.search('/viagra/i') || message.search('/XXX/i') || message.search('/XXX/i')){
+            let filter = '***';
+            let messageFilter = message.replace(/viagra/gi, filter);
+            let messageFilterTwo = messageFilter.replace(/XXX/gi, filter);
+            let messageResult = messageFilterTwo.replace(/XXX/gi, filter);
+            result.innerHTML += `<div><p class="result">${messageResult}</p></div>`;
+        } 
+    }       
 }
 
 let btn = document.querySelector(".button");
-btn.addEventListener("click", addMessage);
+btn.addEventListener("click", checkSpam);
