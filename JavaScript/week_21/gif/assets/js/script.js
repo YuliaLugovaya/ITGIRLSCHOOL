2d173e7f0a2e504eed1dfcb2efb7900980af452c
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-01-09 03:01:10 +0300

    fixed gif

--- JavaScript/week_21/gif/assets/js/script.js
+++ JavaScript/week_21/gif/assets/js/script.js
@@ -6,15 +6,35 @@ let gifs = [];
 btn.addEventListener('click', (e) => {
     e.preventDefault();
 
-    let search = document.querySelector('.searh').value;
+    let search = document.querySelector('.search').value;
     
-    fetch(`https://api.giphy.com/v1/gifs/search?api_key=C0ZP0xR1klDe6ID8tIORjR17tQTEUjYx&limit=5&offset=0&q=${search}`)
+    //с помощью async
+
+    async function loadData(){
+        try {
+            let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=C0ZP0xR1klDe6ID8tIORjR17tQTEUjYx&limit=5&offset=0&q=${search}`);
+            let pictures = await response.json();
+    
+            gifs = pictures.data;
+            gifs.forEach((el) => {
+                add.innerHTML += `<img src="${el.images.fixed_height.url}" alt = "">`;
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    loadData();
+
+    //с помощью промисов
+
+    /*fetch(`https://api.giphy.com/v1/gifs/search?api_key=C0ZP0xR1klDe6ID8tIORjR17tQTEUjYx&limit=5&offset=0&q=${search}`)
     .then(response => response.json())
-    .then(user => {
-        gifs = user.data;
+    .then(pictures => {
+        gifs = pictures.data;
         gifs.forEach((el) => {
-            add.innerHTML += `<img src="${el.images.downsized.url}" alt = "">`;
-          });
+            add.innerHTML += `<img src="${el.images.fixed_height.url}" alt = "">`;
+        });
     })
-    .catch(error => console.log(error));
+    .catch(error => console.log(error));*/
 })
